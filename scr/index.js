e9212805c1d6962bd8fc98ff60391944033834ac
MarkHiarley <hiarleycostamarley@gmail.com>
2024-10-14 22:34:15 -0300

    finalizando eu acho

--- scr/index.js
+++ scr/index.js
@@ -10,6 +10,27 @@ function moveCard() {
     card.style.top = `${randomY}px`;
 }
 
-document.getElementById('sim').addEventListener('click', function() {
+document.getElementById('sim').addEventListener('click', function () {
     window.location.href = '/pages/page.html';
 });
+
+const textoo = "Este é um exemplo de texto que aparece com um delay, como se estivesse sendo digitado.";
+const saindo = document.getElementById('texto');
+let index = 0;
+
+
+
+function typeText() {
+    if (index < text.length) {
+
+
+
+        texto.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeText, 1);
+
+
+    }
+}
+
+typeText();
